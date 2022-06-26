6f95abcf9f5f44037163e71323ceb00cc8968a63
AntCasey <antcasey.developer@gmail.com>
2022-06-26 19:35:15 +0100

    Bypass build issue with a record-based Mixin - use the underlying class instead

--- src/main/java/com/ant/mcskyblock/mixin/MixinConfiguredFeature.java
+++ src/main/java/com/ant/mcskyblock/mixin/MixinConfiguredFeature.java
@@ -7,12 +7,16 @@ import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ConfiguredFeature.class)
-public record MixinConfiguredFeature<FC extends FeatureConfig, F extends Feature<FC>>(F feature, FC config) {
+public class MixinConfiguredFeature<FC extends FeatureConfig, F extends Feature<FC>> {
+    @Shadow
+    private F feature;
+
     @Inject(at = @At("HEAD"), method="generate", cancellable = true)
     public void generate(StructureWorldAccess world, ChunkGenerator chunkGenerator, Random random, BlockPos origin, CallbackInfoReturnable<Boolean> cir) {
         if (chunkGenerator instanceof SkyblockChunkGenerator &&
