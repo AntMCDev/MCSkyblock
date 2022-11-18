dae38e3d6e7187245470db2a4cb0f79900c64418
AntCasey <antcasey.developer@gmail.com>
2022-11-18 21:51:19 +0000

    Fixed iceberg reference

--- src/main/java/com/ant/mcskyblock/mixin/MixinIcebergFeature.java
+++ src/main/java/com/ant/mcskyblock/mixin/MixinIcebergFeature.java
@@ -1,6 +1,6 @@
 package com.ant.mcskyblock.mixin;
 
-import com.ant.mcskyblock.common.config.SkyBlockConfigManager;
+import com.ant.mcskyblock.common.config.SkyBlockConfig;
 import com.ant.mcskyblock.common.world.level.levelgen.SkyBlockChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.IcebergFeature;
@@ -15,7 +15,7 @@ public class MixinIcebergFeature {
     @Inject(at = @At("HEAD"), method = "place", cancellable = true)
     public void place(FeaturePlaceContext<BlockStateConfiguration> featurePlaceContext, CallbackInfoReturnable<Boolean> cir) {
         if (featurePlaceContext.chunkGenerator() instanceof SkyBlockChunkGenerator) {
-            if(!SkyBlockConfigManager.iceBerg()){
+            if(!SkyBlockConfig.Structures.GEN_ICEBERGS){
                 cir.setReturnValue(false);
             }
         }
