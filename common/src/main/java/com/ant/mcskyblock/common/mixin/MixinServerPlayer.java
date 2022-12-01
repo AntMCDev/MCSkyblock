dad99605bf287596ce743b18a656c04b9eb1385b
AntCasey <antcasey.developer@gmail.com>
2022-12-01 21:39:55 +0000

    Fix MixinServerPlayer method header

--- common/src/main/java/com/ant/mcskyblock/common/mixin/MixinServerPlayer.java
+++ common/src/main/java/com/ant/mcskyblock/common/mixin/MixinServerPlayer.java
@@ -1,8 +1,10 @@
 package com.ant.mcskyblock.common.mixin;
 
+import com.ant.mcskyblock.common.SkyBlock;
 import com.ant.mcskyblock.common.world.level.levelgen.IslandGenerator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayer.class)
 public class MixinServerPlayer {
     @Inject(method = "die", at = @At("HEAD"))
-    public void init(CallbackInfo ci) {
+    public void die(DamageSource damageSource, CallbackInfo ci) {
         ServerPlayer player = (ServerPlayer)((Object)this);
         if (player.getRespawnPosition() == null) {
             BlockPos b = IslandGenerator.islandPosition(player.getStringUUID());
-            if (b != null) { player.setRespawnPosition(player.getServer().overworld().dimension(), b, 0, true, false); }
+            if (b != null) { SkyBlock.LOGGER.info(b); player.setRespawnPosition(player.getServer().overworld().dimension(), b, 0, true, false); }
         }
     }
 }
