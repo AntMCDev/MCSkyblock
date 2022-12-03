ae7429db26b5ecfc56a120004deeaf95886e679a
AntCasey <antcasey.developer@gmail.com>
2022-12-03 17:15:21 +0000

    Pushed default biome builder - mod based

--- common/src/main/java/com/ant/mcskyblock/common/config/BiomeIslandConfig.java
+++ common/src/main/java/com/ant/mcskyblock/common/config/BiomeIslandConfig.java
@@ -7,21 +7,36 @@ import java.util.Map;
 
 public class BiomeIslandConfig {
     public static class Island {
+        public String mod = null;
         public ResourceLocation base = null;
         public ResourceLocation fluid = null;
         public ResourceLocation accessory = null;
+
+        public Island() {}
+
+        public Island(String mod, ResourceLocation base, ResourceLocation fluid, ResourceLocation accessory) {
+            this.mod = mod;
+            this.base = base;
+            this.fluid = fluid;
+            this.accessory = accessory;
+        }
     }
 
     public static Map<ResourceLocation, Island> SETTINGS = new HashMap<>();
 
-    public static final String DEFAULT_JSON_STRING = """
-            [
-              {
-                "biome": "minecraft:plains",
-                "base": "minecraft:grass_block",
-                "fluid": "minecraft:water",
-                "accessory": "minecraft:sunflower"
-              }
-            ]
-            """;
+    public static void defaults() {
+        SETTINGS.put(new ResourceLocation("minecraft:plains"), toIsland("vanilla", "grass_block", "water", "sunflower"));
+        //SETTINGS.put(new ResourceLocation("minecraft:plains"), toIsland("vanilla", "grass_block", "water", "sunflower"));
+        //SETTINGS.put(new ResourceLocation("minecraft:plains"), toIsland("vanilla", "grass_block", "water", "sunflower"));
+        //SETTINGS.put(new ResourceLocation("minecraft:plains"), toIsland("vanilla", "grass_block", "water", "sunflower"));
+    }
+
+    private static Island toIsland(String mod, String base, String fluid, String accessory) {
+        return new Island(
+                mod,
+                base == null ? null : new ResourceLocation(base),
+                fluid == null ? null : new ResourceLocation(fluid),
+                accessory == null ? null : new ResourceLocation(accessory)
+        );
+    }
 }
--- common/src/main/java/com/ant/mcskyblock/common/config/ConfigFileAccessor.java
+++ common/src/main/java/com/ant/mcskyblock/common/config/ConfigFileAccessor.java
@@ -3,17 +3,13 @@ package com.ant.mcskyblock.common.config;
 import com.ant.mcskyblock.common.SkyBlock;
 import com.ant.mcskyblock.common.registry.RegistryAccess;
 import com.google.gson.*;
-import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceLocation;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfigFileAccessor {
     public static Path path = null;
@@ -50,78 +46,104 @@ public class ConfigFileAccessor {
 
     public static void saveBiomes() {
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        final Path configPath = path.resolve(SkyBlock.MOD_NAME + "-biomes.json");
+        final Path configPath = path.resolve(SkyBlock.MOD_NAME + "/biomes");
+
+        // Split flatmap into a collection of mod-based maps
+        Map<String, Map<ResourceLocation, BiomeIslandConfig.Island>> configMap = new HashMap<>();
+        BiomeIslandConfig.SETTINGS.keySet().forEach(rs -> {
+            BiomeIslandConfig.Island i = BiomeIslandConfig.SETTINGS.get(rs);
+            if (!configMap.containsKey(i.mod)) {
+                configMap.put(i.mod, new HashMap<>());
+            }
+            configMap.get(i.mod).put(rs, i);
+        });
 
         try {
-            Files.createDirectories(configPath.getParent());
-            BufferedWriter writer = Files.newBufferedWriter(configPath);
-            JsonArray arr = new JsonArray();
-            for (ResourceLocation rs : BiomeIslandConfig.SETTINGS.keySet()) {
-                JsonObject obj = new JsonObject();
-                BiomeIslandConfig.Island island = BiomeIslandConfig.SETTINGS.get(rs);
-                obj.addProperty("biome", rs.toString());
-                if (island.base != null) { obj.addProperty("base", island.base.toString()); }
-                if (island.fluid != null) { obj.addProperty("fluid", island.fluid.toString()); }
-                if (island.accessory != null) { obj.addProperty("accessory", island.accessory.toString()); }
-                arr.add(obj);
-            }
-            gson.toJson(arr, writer);
-            writer.close();
+            // Create biomes directory
+            Files.createDirectories(configPath);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        // Loop over each mod set
+        for (String s : configMap.keySet()) {
+            try {
+                // Create mod file
+                Files.createDirectories(configPath.resolve(s + ".json"));
+
+                // Write JSON
+                BufferedWriter writer = Files.newBufferedWriter(configPath);
+                JsonArray arr = new JsonArray();
+                Map<ResourceLocation, BiomeIslandConfig.Island> settings = configMap.get(s);
+                for (ResourceLocation rs : settings.keySet()) {
+                    JsonObject obj = new JsonObject();
+                    BiomeIslandConfig.Island island = settings.get(rs);
+                    obj.addProperty("biome", rs.toString());
+                    if (island.base != null) { obj.addProperty("base", island.base.toString()); }
+                    if (island.fluid != null) { obj.addProperty("fluid", island.fluid.toString()); }
+                    if (island.accessory != null) { obj.addProperty("accessory", island.accessory.toString()); }
+                    arr.add(obj);
+                }
+                gson.toJson(arr, writer);
+                writer.close();
+            } catch (IOException ignore) {}
         }
     }
 
     public static void loadBiomes() {
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        final Path configPath = path.resolve(SkyBlock.MOD_NAME + "-biomes.json");
+        final Path configPath = path.resolve(SkyBlock.MOD_NAME + "/biomes");
 
         if (Files.exists(configPath)) {
-            try {
-                BufferedReader reader = Files.newBufferedReader(configPath);
-                parseBiomes(gson.fromJson(reader, JsonArray.class));
-                reader.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            File[] files = configPath.toFile().listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    try {
+                        BufferedReader reader = Files.newBufferedReader(f.toPath());
+                        JsonArray arr = gson.fromJson(reader, JsonArray.class);
+                        arr.forEach(j -> {
+                            JsonObject obj = (JsonObject)j;
+                            ResourceLocation biome = new ResourceLocation(obj.get("biome").getAsString());
+                            if (RegistryAccess.INSTANCE.getBiome(biome) != null) {
+                                BiomeIslandConfig.Island island = new BiomeIslandConfig.Island();
+
+                                island.mod = f.getName().replace(".json", "");
+
+                                JsonElement baseElem = obj.get("base");
+                                if (baseElem != null) {
+                                    ResourceLocation base = new ResourceLocation(baseElem.getAsString());
+                                    if (RegistryAccess.INSTANCE.getBlock(base) != null) {
+                                        island.base = base;
+                                    }
+                                }
+
+                                JsonElement baseFluid = obj.get("fluid");
+                                if (baseFluid != null) {
+                                    ResourceLocation fluid = new ResourceLocation(obj.get("fluid").getAsString());
+                                    if (RegistryAccess.INSTANCE.getBlock(fluid) != null) {
+                                        island.fluid = fluid;
+                                    }
+                                }
+
+                                JsonElement baseAccessory = obj.get("accessory");
+                                if (baseAccessory != null) {
+                                    ResourceLocation accessory = new ResourceLocation(obj.get("accessory").getAsString());
+                                    if (RegistryAccess.INSTANCE.getBlock(accessory) != null) {
+                                        island.accessory = accessory;
+                                    }
+                                }
+
+                                BiomeIslandConfig.SETTINGS.put(biome, island);
+                            }
+                        });
+                        reader.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         } else {
-            parseBiomes(gson.fromJson(new StringReader(BiomeIslandConfig.DEFAULT_JSON_STRING), JsonArray.class));
+            BiomeIslandConfig.defaults();
         }
-    }
-
-    private static void parseBiomes(JsonArray arr) {
-        arr.forEach(j -> {
-            JsonObject obj = (JsonObject)j;
-            ResourceLocation biome = new ResourceLocation(obj.get("biome").getAsString());
-            if (RegistryAccess.INSTANCE.getBiome(biome) != null) {
-                BiomeIslandConfig.Island island = new BiomeIslandConfig.Island();
-
-                JsonElement baseElem = obj.get("base");
-                if (baseElem != null) {
-                    ResourceLocation base = new ResourceLocation(baseElem.getAsString());
-                    if (RegistryAccess.INSTANCE.getBlock(base) != null) {
-                        island.base = base;
-                    }
-                }
-
-                JsonElement baseFluid = obj.get("fluid");
-                if (baseFluid != null) {
-                    ResourceLocation fluid = new ResourceLocation(obj.get("fluid").getAsString());
-                    if (RegistryAccess.INSTANCE.getBlock(fluid) != null) {
-                        island.fluid = fluid;
-                    }
-                }
-
-                JsonElement baseAccessory = obj.get("accessory");
-                if (baseAccessory != null) {
-                    ResourceLocation accessory = new ResourceLocation(obj.get("accessory").getAsString());
-                    if (RegistryAccess.INSTANCE.getBlock(accessory) != null) {
-                        island.accessory = accessory;
-                    }
-                }
-
-                BiomeIslandConfig.SETTINGS.put(biome, island);
-            }
-        });
     }
 }
